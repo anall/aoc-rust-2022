7adf6fdb398e190a5bccaf738557426ab259fc18
Andrea Nall <anall@andreanall.com>
2022-12-07 10:32:39 -0600

    back days

---
+++ src/day4.rs
@@ -0,0 +1,66 @@
+#![warn(clippy::pedantic)]
+use adventlib::aoc;
+use regex::Regex;
+use std::io::BufRead;
+
+struct Span(u32, u32);
+
+impl Span {
+    fn fully_contains(&self, other: &Self) -> bool {
+        other.0 >= self.0 && other.1 <= self.1
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        other.0 >= self.0 && other.0 <= self.1 || other.1 >= self.0 && other.1 <= self.1
+    }
+}
+
+fn solve(filename: &str) -> aoc::Result<(usize, usize)> {
+    let reader = aoc::file(filename)?;
+    let parse_regex = Regex::new(r"^(\d+)-(\d+),(\d+)-(\d+)$").unwrap();
+
+    let spans: Vec<_> = reader
+        .lines()
+        .map(|d| {
+            let line = d.unwrap();
+            let captures = parse_regex.captures(&line).unwrap();
+            let span1 = Span(captures[1].parse().unwrap(), captures[2].parse().unwrap());
+            let span2 = Span(captures[3].parse().unwrap(), captures[4].parse().unwrap());
+
+            (span1, span2)
+        })
+        .collect();
+
+    Ok((
+        spans
+            .iter()
+            .filter(|(span1, span2)| span1.fully_contains(span2) || span2.fully_contains(span1))
+            .count(),
+        spans
+            .iter()
+            .filter(|(span1, span2)| span1.overlaps(span2) || span2.overlaps(span1))
+            .count(),
+    ))
+}
+
+fn main() -> aoc::Result<()> {
+    let (part1, part2) = solve("inputs/day4")?;
+
+    println!("{}", part1);
+    println!("{}", part2);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use crate::solve;
+
+    #[test]
+    fn sample() {
+        let (part1, part2) = solve("inputs-sample/day4").unwrap();
+
+        assert_eq!(part1, 2);
+        assert_eq!(part2, 4);
+    }
+}
---
+++ src/day5.rs
@@ -0,0 +1,82 @@
+#![warn(clippy::pedantic)]
+use adventlib::aoc;
+use regex::Regex;
+use std::{collections::VecDeque, io::BufRead};
+
+fn solve(filename: &str) -> aoc::Result<(String, String)> {
+    let reader = aoc::file(filename)?;
+    let parse_regex = Regex::new(r"(?:\[(.)\]|   )(?: |$)").unwrap();
+    let move_regex = Regex::new(r"^move (\d+) from (\d+) to (\d+)$").unwrap();
+
+    let mut lines = reader.lines();
+    let mut has_capture = true;
+    let mut stacks: Vec<VecDeque<char>> = Vec::new();
+
+    while has_capture {
+        has_capture = false;
+        let line = lines.next().unwrap()?;
+        for (i, cap) in parse_regex.captures_iter(&line).enumerate() {
+            has_capture = true;
+            if i >= stacks.len() {
+                stacks.push(VecDeque::new());
+            }
+            if let Some(val) = cap.get(1) {
+                stacks[i].push_front(val.as_str().chars().next().unwrap());
+            }
+        }
+    }
+
+    assert_eq!(lines.next().unwrap()?, "");
+
+    let mut stacks2 = stacks.clone();
+
+    for line in lines {
+        let line = line.unwrap();
+        let cap = move_regex
+            .captures(&line)
+            .expect("Failed to match move line");
+        let n = cap[1].parse::<usize>().unwrap();
+        let src = cap[2].parse::<usize>().unwrap() - 1;
+        let dest = cap[3].parse::<usize>().unwrap() - 1;
+        assert!(src < stacks.len());
+        assert!(dest < stacks.len());
+
+        let mut tmp: Vec<char> = Vec::new();
+        for _ in 0..n {
+            let item = stacks[src].pop_back().unwrap();
+            stacks[dest].push_back(item);
+
+            tmp.push(stacks2[src].pop_back().unwrap());
+        }
+        for item in tmp.into_iter().rev() {
+            stacks2[dest].push_back(item);
+        }
+    }
+
+    Ok((
+        stacks.iter().map(|v| v.back().unwrap()).collect(),
+        stacks2.iter().map(|v| v.back().unwrap()).collect(),
+    ))
+}
+
+fn main() -> aoc::Result<()> {
+    let (part1, part2) = solve("inputs/day5")?;
+
+    println!("{}", part1);
+    println!("{}", part2);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use crate::solve;
+
+    #[test]
+    fn sample() {
+        let (part1, part2) = solve("inputs-sample/day5").unwrap();
+
+        assert_eq!(part1, "CMZ");
+        assert_eq!(part2, "MCD");
+    }
+}
