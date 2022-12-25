a7a19e753d408f9ed718446867875c595cb1b3d9
Andrea Nall <anall@andreanall.com>
2022-12-25 13:48:07 -0600

    clippy + fmt

--- src/day25.rs
+++ src/day25.rs
@@ -3,23 +3,25 @@
 use adventlib::aoc;
 use std::io::BufRead;
 
-fn convert(number : &[u8]) -> i64 {
+fn convert(number: &[u8]) -> i64 {
     let mut mul = 1;
     let mut result = 0;
-    for val in number.into_iter().rev() {
-        result += mul * match val {
-            b'-' => -1,
-            b'=' => -2,
-            num => (num - b'0') as i64
-        };
+    for val in number.iter().rev() {
+        result += mul
+            * match val {
+                b'-' => -1,
+                b'=' => -2,
+                num => i64::from(num - b'0'),
+            };
         mul *= 5;
     }
 
     result
 }
 
-fn unconvert(mut number : i64) -> Vec<u8> {
-    let mut out : Vec<u8> = Vec::new();
+#[allow(clippy::cast_sign_loss)]
+fn unconvert(mut number: i64) -> Vec<u8> {
+    let mut out: Vec<u8> = Vec::new();
     let mut borrowed = 0;
 
     while number > 0 {
@@ -27,20 +29,20 @@ fn unconvert(mut number : i64) -> Vec<u8> {
         if cur_val > 2 {
             borrowed = 1;
             if cur_val == 3 {
-                out.push(b'=')
+                out.push(b'=');
             } else if cur_val == 4 {
-                out.push(b'-')
+                out.push(b'-');
             } else {
-                unreachable!()
+                unreachable!();
             }
         } else {
             borrowed = 0;
-            out.push( cur_val + b'0' );
+            out.push(cur_val + b'0');
         }
         number /= 5;
     }
     out.reverse();
-    
+
     out
 }
 
@@ -50,9 +52,9 @@ fn solve(filename: &str) -> aoc::Result<(i64, String)> {
     let sum = reader.lines().map(|v| convert(v.unwrap().as_bytes())).sum();
     let snafu_sum = unsafe { String::from_utf8_unchecked(unconvert(sum)) };
 
-    assert_eq!( convert(snafu_sum.as_bytes()), sum );
+    assert_eq!(convert(snafu_sum.as_bytes()), sum);
 
-    Ok( (sum,snafu_sum) )
+    Ok((sum, snafu_sum))
 }
 
 fn main() -> aoc::Result<()> {
@@ -66,7 +68,7 @@ fn main() -> aoc::Result<()> {
 
 #[cfg(test)]
 mod test {
-    use crate::{convert,solve};
+    use crate::{convert, solve};
 
     #[test]
     fn sample() {
@@ -78,18 +80,18 @@ mod test {
 
     #[test]
     fn conversions() {
-        assert_eq!( convert(b"1=-0-2"), 1747 );
-        assert_eq!( convert(b"12111"), 906 );
-        assert_eq!( convert(b"2=0="), 198 );
-        assert_eq!( convert(b"21"), 11 );
-        assert_eq!( convert(b"2=01"), 201 );
-        assert_eq!( convert(b"111"), 31 );
-        assert_eq!( convert(b"20012"), 1257 );
-        assert_eq!( convert(b"112"), 32 );
-        assert_eq!( convert(b"1=-1="), 353 );
-        assert_eq!( convert(b"1-12"), 107 );
-        assert_eq!( convert(b"12"), 7 );
-        assert_eq!( convert(b"1="), 3 );
-        assert_eq!( convert(b"122"), 37 );
+        assert_eq!(convert(b"1=-0-2"), 1747);
+        assert_eq!(convert(b"12111"), 906);
+        assert_eq!(convert(b"2=0="), 198);
+        assert_eq!(convert(b"21"), 11);
+        assert_eq!(convert(b"2=01"), 201);
+        assert_eq!(convert(b"111"), 31);
+        assert_eq!(convert(b"20012"), 1257);
+        assert_eq!(convert(b"112"), 32);
+        assert_eq!(convert(b"1=-1="), 353);
+        assert_eq!(convert(b"1-12"), 107);
+        assert_eq!(convert(b"12"), 7);
+        assert_eq!(convert(b"1="), 3);
+        assert_eq!(convert(b"122"), 37);
     }
 }
